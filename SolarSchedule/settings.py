862981d919f93fba2d0ba293ab764ebb58e70f73
Reka Alexsandr <nnicrow@gmail.com>
2024-02-19 21:29:31 +0300

    fix

--- SolarSchedule/settings.py
+++ SolarSchedule/settings.py
@@ -136,6 +136,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'solar_schedule.Users'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
